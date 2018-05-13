a92a625922d5ad8f2a7806ce7b60530a8abcc837
xiexinran <781473919@qq.com>
2018-05-13 17:13:02 +0800

    Finish basic page layout

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,3 +5,7 @@
  * Date: 2018/5/13
  * Time: 16:10
  */
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            <a href="http://www.cuit.edu.cn/" target="_blank">成都信息工程大学</a>
+        </p>
+
+        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
