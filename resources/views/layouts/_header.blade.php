1bce0c227d2d4490014f22003fc79256f8149dbe
xiexinran <781473919@qq.com>
2018-05-13 22:43:05 +0800

    Basic user profile editing

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -38,6 +38,13 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
+
+                            <li>
+                                <a href="{{ route('users.edit', Auth::id()) }}">
+                                    编辑资料
+                                </a>
+                            </li>
+
                             <li>
                                 <a href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
@@ -50,6 +57,9 @@
                                 </form>
                             </li>
                         </ul>
+
+
+
                     </li>
                 @endguest
             </ul>
