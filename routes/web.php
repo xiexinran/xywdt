b36301857149ce12774b6246abbb3673565ea6fe
xiexinran <781473919@qq.com>
2018-05-14 13:29:02 +0800

    Generated topics

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 
 //个人资料展示、更新、编辑路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
