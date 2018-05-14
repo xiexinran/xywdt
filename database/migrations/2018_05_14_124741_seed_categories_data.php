40c16030e4abc6ead183ed54a2faa2d136dfec97
xiexinran <781473919@qq.com>
2018-05-14 12:51:36 +0800

    Create categories

---
+++ database/migrations/2018_05_14_124741_seed_categories_data.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SeedCategoriesData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $categories = [
+            [
+                'name'        => '记录生活',
+                'description' => '分享点滴，记录生活',
+            ],
+            [
+                'name'        => '活动',
+                'description' => '参与学校发布线上活动',
+            ],
+            [
+                'name'        => '问答',
+                'description' => '请保持友善，互帮互助',
+            ],
+            [
+                'name'        => '公告',
+                'description' => '站点公告',
+            ],
+        ];
+
+        DB::table('categories')->insert($categories);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('categories')->truncate();
+    }
+}
